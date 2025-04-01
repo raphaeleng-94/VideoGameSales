e455a864af8357796c32276cf00f4c811bd02990
raphaeleng-94 <raphaelproge@gmail.com>
2025-04-01 17:33:09 -0300

    Projeto atualizado

--- data/load_csv.py
+++ data/load_csv.py
@@ -7,5 +7,7 @@ file_path = './data/vgsales.csv'
 df = pd.read_csv(file_path)
 
 # Exibindo as colunas e tipos
-print(df.dtypes)
-print(df.head())
+"""print(df.dtypes)
+print(df.head())"""
+print(f"Linhas: {df.shape[0]}, Colunas: {df.shape[1]}")
+print(f"Tipo das colunas: {df.dtypes}")
